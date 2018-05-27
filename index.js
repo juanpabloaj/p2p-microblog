2a42976f484473121a2aa481cbc85270f3011d19
JuanPablo <jpabloaj@gmail.com>
2018-05-27 12:25:53 -0400

    sources from json

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const choo = require('choo')
 const html = require('choo/html')
+const dat = require('./lib/dat.js')
 
 var mainView = require('./templates/main.js')
 
@@ -25,11 +26,20 @@ app.use(function (state, emitter) {
 
   emitter.on('addSource', function (data) {
     state.sources.push(data)
+    dat.writeJson(
+      window.location.toString(),
+      '/sources.json', {sources: state.sources}
+    )
     emitter.emit('render')
   })
 
   emitter.on('removeSource', function (i) {
     state.sources.splice(i, 1)
+    emitter.emit('render')
+  })
+
+  dat.loadJson(window.location.toString(), '/sources.json').then(json => {
+    state.sources = json.sources
     emitter.emit('render')
   })
 })
